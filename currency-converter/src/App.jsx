62497373a240f571fa8fe9d640c6906a81d10c16
codeby-rafay <rafayali146@gmail.com>
2026-04-01 21:12:41 +0500

    first commit

--- currency-converter/src/App.jsx
+++ currency-converter/src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div>
-      <h1 className="bg-black text-white text-4xl font-bold flex items-center justify-center">
+      <h1 className="bg-black text-white text-4xl font-bold flex items-center justify-center p-4">
         Currency Converter
       </h1>
     </div>
