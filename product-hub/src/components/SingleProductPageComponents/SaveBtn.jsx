fa7e6500c2c738696464661eadc8b5a00389d88d
codeby-rafay <rafayali146@gmail.com>
2026-04-02 21:34:09 +0500

    Wishlist button updated

--- product-hub/src/components/SingleProductPageComponents/SaveBtn.jsx
+++ product-hub/src/components/SingleProductPageComponents/SaveBtn.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import { useState } from 'react';
 
 const SaveBtn = () => {
+  const [isSaved, setIsSaved] = useState(false);
+
+  const ClickSave = () => {
+    setIsSaved(!isSaved);
+  };
+
   return (
     <div>
-      <button className="bg-gray-100 hover:bg-gray-200 text-gray-700 py-3 px-4 flex items-center gap-2 cursor-pointer rounded-xl transition-colors">
+      <button
+        onClick={ClickSave}
+        className={`py-3 px-4 flex items-center gap-2 cursor-pointer rounded-xl transition-colors ${
+          isSaved
+            ? 'bg-red-100 hover:bg-red-200 text-red-600'
+            : 'bg-gray-100 hover:bg-gray-200 text-gray-700'
+        }`}
+      >
         <img
           className="size-4"
-          src="https://www.svgrepo.com/show/447850/wishlist.svg"
+          src={
+            isSaved
+              ? 'https://www.svgrepo.com/show/535436/heart.svg'
+              : 'https://www.svgrepo.com/show/447850/wishlist.svg'
+          }
           alt="Wishlist"
-        />{" "}
-        Save
+        />
+        {isSaved ? 'Saved' : 'Save'}
       </button>
     </div>
   );
