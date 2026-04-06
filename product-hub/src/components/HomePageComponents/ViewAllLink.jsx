1b8b739a0afc1162855cf6469bf0d06926991d8c
codeby-rafay <rafayali146@gmail.com>
2026-04-06 21:34:44 +0500

    view all link updated

--- product-hub/src/components/HomePageComponents/ViewAllLink.jsx
+++ product-hub/src/components/HomePageComponents/ViewAllLink.jsx
@@ -2,26 +2,28 @@ import { Link } from "react-router-dom";
 
 const ViewAllLink = () => {
   return (
-    <div><Link
-            to="/products"
-            className="text-orange-500 hover:text-orange-600 text-lg font-bold flex items-center gap-1"
-          >
-            View all
-            <svg
-              className="w-4 h-4"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M9 5l7 7-7 7"
-              />
-            </svg>
-          </Link></div>
-  )
-}
+    <div>
+      <Link
+        to="/products"
+        className="text-orange-500 hover:text-orange-600 text-lg font-bold flex items-center gap-1"
+      >
+        View all
+        <svg
+          className="w-4 h-4"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M9 5l7 7-7 7"
+          />
+        </svg>
+      </Link>
+    </div>
+  );
+};
 
-export default ViewAllLink
+export default ViewAllLink;
