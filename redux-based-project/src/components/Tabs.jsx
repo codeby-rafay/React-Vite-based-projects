654f6f787756e438cd4c722ad6e1b6a7f128f684
codeby-rafay <rafayali146@gmail.com>
2026-04-14 14:49:03 +0500

    ui Updated

--- redux-based-project/src/components/Tabs.jsx
+++ redux-based-project/src/components/Tabs.jsx
@@ -15,11 +15,11 @@ const Tabs = () => {
               key={idx}
               onClick={() => dispatch(setActiveTab(elem))}
               className={`
-                px-8 py-3 font-semibold text-lg rounded-lg transition-all duration-300 uppercase cursor-pointer
+                px-8 py-3 font-semibold text-lg rounded-lg transition duration-300 uppercase cursor-pointer
                 transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800
                 ${
                   activeTab === elem
-                    ? "bg-linear-to-r from-blue-500 to-blue-600 text-white shadow-lg shadow-blue-500/50 scale-105"
+                    ? "bg-linear-to-r from-blue-500 text-white shadow-lg shadow-blue-500/50 scale-105"
                     : "bg-gray-700 text-gray-200 hover:bg-gray-600 hover:text-white"
                 }
               `}
