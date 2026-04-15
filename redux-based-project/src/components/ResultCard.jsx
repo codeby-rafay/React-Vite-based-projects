612ae061692745d80583aacb3adc80c8dd26c609
codeby-rafay <rafayali146@gmail.com>
2026-04-15 19:36:36 +0500

    save button updated

--- redux-based-project/src/components/ResultCard.jsx
+++ redux-based-project/src/components/ResultCard.jsx
@@ -1,16 +1,9 @@
-import { Bookmark } from "lucide-react";
-import { useState } from "react";
-
 const ResultCard = ({ item }) => {
-  const [isBookmarked, setIsBookmarked] = useState(false);
-
   const handleBookmark = (item) => {
-    item.preventDefault();
-    setIsBookmarked(!isBookmarked);
-
     const oldData = JSON.parse(localStorage.getItem("collection")) || [];
     const newData = [...oldData, item];
     localStorage.setItem("collection", JSON.stringify(newData));
+    console.log(newData);
   };
 
   return (
@@ -37,23 +30,12 @@ const ResultCard = ({ item }) => {
         <div className="absolute inset-0 bg-linear-to-t from-black via-transparent to-transparent opacity-40 group-hover:opacity-60 transition-opacity duration-300"></div>
 
         <div className="absolute bottom-0 left-0 right-0 p-4 bg-linear-to-t from-black/80 to-transparent text-white">
-          <div className="flex justify-between items-start gap-2">
+          <div className="flex justify-between items-center gap-2">
             <h1 className="text-sm font-bold capitalize line-clamp-2 flex-1">
               {item.title}
             </h1>
-            <button
-              onClick={handleBookmark}
-              className={`p-2 rounded-full transition-all duration-300 cursor-pointer ${
-                isBookmarked
-                  ? "bg-blue-300 text-black"
-                  : "bg-white/20 hover:bg-white/40 text-white"
-              }`}
-              title={isBookmarked ? "Remove bookmark" : "Add bookmark"}
-            >
-              <Bookmark
-                size={20}
-                fill={isBookmarked ? "currentColor" : "none"}
-              />
+            <button onClick={() => handleBookmark(item)} className="px-2 py-1 rounded transition-all duration-300 cursor-pointer bg-white/20 hover:bg-white/40 text-white font-bold">
+              Save
             </button>
           </div>
         </div>
