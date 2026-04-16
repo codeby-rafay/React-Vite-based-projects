a8790bb57604478cd330dbe3581e078a7604c8e2
codeby-rafay <rafayali146@gmail.com>
2026-04-16 15:16:21 +0500

    link created at logo

--- media-search-redux-project/src/components/Navbar.jsx
+++ media-search-redux-project/src/components/Navbar.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="flex justify-between py-6 px-10 items-center">
-      <h2 className="text-2xl font-bold">
+      <Link className="text-2xl font-bold" to="/">
         Media
         <span className="text-transparent bg-clip-text bg-linear-to-r from-blue-500 to-purple-500">
           Search
         </span>
-      </h2>
+      </Link>
       <div className="flex gap-5 items-center">
         <Link
           className="text-base font-medium text-blue-800 bg-white rounded px-3 py-1 active:scale-95"
