672d21e0d1e75f6826d80d0acace0641c3984024
codeby-rafay <rafayali146@gmail.com>
2026-06-05 16:43:03 +0500

    console error statemnts added in components

--- product-hub-ecomm-store/frontend/src/components/CartDisplay.jsx
+++ product-hub-ecomm-store/frontend/src/components/CartDisplay.jsx
@@ -135,6 +135,7 @@ function CartDisplay() {
         }, 500);
       }
     } catch (error) {
+      console.error("Error during checkout:", error);
       toast.error(
         error.response?.data?.message ||
           error.message ||
